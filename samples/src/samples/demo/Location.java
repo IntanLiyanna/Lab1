e92289788456d7062e42d949fc70a786b64de50a
IntanLiyanna <intanliyanna@gmail.com>
2022-03-10 23:56:56 +0800

    Third Commit

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,13 @@
+package samples.demo;
+
+public class Location {
+	private int numberphone;
+
+	public int getNumberphone() {
+		return numberphone;
+	}
+
+	public void setNumberphone(int numberphone) {
+		this.numberphone = numberphone;
+	}
+}
