470fec83eb62e2b99fc32dccc833e06c90a005ac
IntanLiyanna <intanliyanna@gmail.com>
2022-03-10 23:59:33 +0800

    Fifth Commit

--- samples/src/samples/demo/Location.java
+++ samples/src/samples/demo/Location.java
@@ -1,16 +1,7 @@
 package samples.demo;
 
 public class Location {
-	private int numberphone;
 	private String address;
-
-	public int getNumberphone() {
-		return numberphone;
-	}
-
-	public void setNumberphone(int numberphone) {
-		this.numberphone = numberphone;
-	}
 
 	public String getAddress() {
 		return address;
